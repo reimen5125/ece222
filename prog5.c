d49a4e5cdc161fc9473288b87de54302c520419a
reimen5125 <reimen@g.clemson.edu>
2017-04-14 21:12:04 -0400

    Upload 9
    It works!! Yay!

--- prog5.c
+++ prog5.c
@@ -39,6 +39,7 @@ int main ( int argc, char **argv )
 		char **notDict;
 		int contains;
 		char **ousArr;
+		char ousStr[4];
 		char **letArr;
 		int letCount;
 		int notCount;
@@ -118,9 +119,11 @@ int main ( int argc, char **argv )
 
 					// Dictionary Check
 						contains = 0;
+						str[strlen(str) + 1] = '\0';
 						for (i = 0; i < dictLen; i++)
 						{
 							if (strstr(dictArr[i], str) != NULL)
+					//		if (strcmp(dictArr[i], str) == 0)
 								contains = 1;
 						}
 
@@ -129,6 +132,7 @@ int main ( int argc, char **argv )
 							for (i = 0; i < j; i++)
 							{
 								if (strstr(notDict[i], str) != NULL)
+						//		if (strcmp(notDict[i], str) == 0)
 								{
 									dictNum[i]++;
 									contains = 2;
@@ -139,7 +143,7 @@ int main ( int argc, char **argv )
 							{
 								notDict[j] = (char *)malloc(46*sizeof(char));
 								strcpy(notDict[j], str);
-					//			printf("NEW STRING_%d  %s\n",j , notDict[j]);
+						//		printf("NEW STRING_%d  %s\n",j , notDict[j]);
 								dictNum[i]++;
 								j++;
 							}
@@ -148,10 +152,21 @@ int main ( int argc, char **argv )
 
 					// "OUS" Check
 						contains = 0;
-						if (strstr(str, "ous") != NULL)
+				//		strcpy(ousStr, "ous");
+				//		str[strlen(str) + 1] = '\0';
+				//		if (strstr(str, ousStr) != NULL)
+				//		printf("%s", str);
+				/*		for (i = 0; i < sizeof(str) - 2; i++)
 						{
-							printf("testing testing");
-							printf("ousArr[%d] = %s\n", ousCount, ousArr[ousCount]);
+							if (str[i] == 'o' && str[i + 1] == 'u' && str[i + 2] == 's')
+								contains = 2;
+						}
+				*/
+						if (strstr(str, "OUS") != NULL)
+				//		if (contains == 2)
+						{
+						//	printf("testing testing");
+						//	printf("ousArr[%d] = %s\n", ousCount, ousArr[ousCount]);
 							for (i = 0; i < ousCount; i++)
 							{
 								if (strstr(ousArr[i], str) != NULL)
@@ -161,13 +176,13 @@ int main ( int argc, char **argv )
 							{
 								ousArr[ousCount] = (char *)malloc(46*sizeof(char));
 								strcpy(ousArr[ousCount], str);
-								printf("ousArr[%d] = %s\n", ousCount, ousArr[ousCount]);
+						//		printf("ousArr[%d] = %s\n", ousCount, ousArr[ousCount]);
 								ousCount++;
 							}
 						}
 
 					// ACESLMTW Check
-						if (strspn(str, "ACESLMTWaceslmtw") == strlen(str))
+						if (strspn(str, "ACESLMTW") == strlen(str))
 						{
 							letArr[letCount] = (char *)malloc(46*sizeof(char));
 							strcpy(letArr[letCount], str);
@@ -214,34 +229,44 @@ int main ( int argc, char **argv )
 		file = fopen(fileName, "w");
 
 		fprintf(file, "Total Novel Words = %d\n\n", wordCount);
+	//	printf("Total Novel Words = %d\n\n", wordCount);
+		fprintf(file, "Total Novel Letters = %d\n\n", charCount);
+	//	printf("Total Novel Letters = %d\n\n", charCount);
 		for (i = 0; i < 26; i++)
 		{
 			fprintf(file, "Total Letters %c = %d = %4.2f%%\n", (char)(i + 65), charFreq[i], (float)charFreq[i]/charCount*100);
+	//		printf("Total Letters %c = %d = %4.2f%%\n", (char)(i + 65), charFreq[i], (float)charFreq[i]/charCount*100);
 		}
 		fprintf(file, "\n");
+	//	printf("\n");
 		for (i = 1; i < 46; i++)
 		{
 			if (sizeFreq[i] > 0)
 			{
 				fprintf(file, "Total Words of Length %d = %d\n", i, sizeFreq[i]);
+	//			printf("Total Words of Length %d = %d\n", i, sizeFreq[i]);
 			}
 		}
 		fprintf(file, "\nAddendum Words:\n");
+		printf("\nAppendum Words:\n");
 		for (i = 0; i < notCount; i++)
 		{
 			fprintf(file, "Word[%d] = %s [%d]\n", i, notDict[i], dictNum[i]);
 			printf("Word[%d] = %s [%d]\n", i, notDict[i], dictNum[i]);
 		}
 		fprintf(file, "\nWords containing the string \"OUS\"\n");
+	//	printf("\nWords containing the string \"OUS\"\n");
 		for (i = 0; i < ousCount; i++)
 		{
-			fprintf(file, "SubStringWord[%d] = %s", i, ousArr[i]);
+			fprintf(file, "SubStringWord[%d] = %s\n", i, ousArr[i]);
+	//		printf("SubStringWord[%d] = %s\n", i, ousArr[i]);
 		}
 		fprintf(file, "\nWords only made up of the letters \"ACESLMTW\"\n");
+	//	printf("\nWords only made up of the letters \"ACESLMTW\"\n");
 		for (i = 0; i < letCount; i++)
 		{
 			fprintf(file, "SpecialWord[%d] = %s\n", i, letArr[i]);
-			printf("SpecialWord[%d] = %s\n", i, letArr[i]);
+	//		printf("SpecialWord[%d] = %s\n", i, letArr[i]);
 		}
 
 		fclose(file);

36a7b7fba67baae5b12bdedcbe5e9111c26aa91d
reimen5125 <reimen@g.clemson.edu>
2017-04-27 22:28:58 -0400

    Upload 3
    Parent Operation; Child Operation (needs perfection with missile information and successful/unsuccessful mission statement)

--- prog6.c
+++ prog6.c
@@ -14,17 +14,13 @@
 #include <sys/types.h>
 #include <stdlib.h>
 #include <string.h>
-
-//FILE *popen(const char *command, const char *mode);
-//int pclose(FILE *stream);
+#include <math.h>
 
 int main(void)
 {
 	// Variable Definition
 		FILE *file;
 		char var[20];
-//		int num = 0;
-//		char temp[46];
 		time_t t;
 		int sub;
 		int parent;
@@ -35,19 +31,25 @@ int main(void)
 		int i;
 		int found;
 		int *term;
-//		char loc1[20];
-//		char loc2[20];
-//		char loc3[20];
-//		char loc4[20];
+		int cont;
+		int fuel;
+		int dist_go;
+		int dist_ret;
+		int missiles;
+		int ret = 0;
+		struct tm* tm_info;
+		char outTime[26];
+
+	// Memory allocation
 		term = (int *)calloc(4, sizeof(int));
+
 	// Terminal number(s) find
-		system("ls -l /dev/pts");
+//		system("ls -l /dev/pts");
 
 		found = 0;
 		i = 0;
 		while (i < 2000 && found < 4)
 		{
-			// num = system("ls -l /dev/pts");
 			sprintf(var, "/dev/pts/%d", i);
 			file = fopen(var, "r");
 			if (file != NULL)
@@ -65,7 +67,7 @@ int main(void)
 		{
 			sprintf(var, "/dev/pts/%d", term[i]);
 			file = fopen(var, "w");
-			fprintf(file, "testing, testing\n");
+//			fprintf(file, "testing, testing\n");
 			if (file != NULL)
 				fclose(file);
 		}
@@ -73,6 +75,16 @@ int main(void)
 	// Print start date and time of the mission
 		time(&t);
 		printf("Date and time:  %s\n", ctime(&t));
+
+	// Variables for children
+		srand(time(NULL));
+		fuel = rand() % 4000 + 1000;	// initial diesel level (1000-5000) [gallons]
+		srand(time(NULL) + 1);
+		missiles = rand() % 4 + 6;	// initial missiles (5-10) [-]
+		dist_go = 0;	// initial distance from base (0) [miles]
+		sprintf(var, "/dev/pts/%d", term[2]);
+		file = fopen(var, "w");
+		cont = 1;
 
 	// Fork children and assign pid's
 		sub = fork();
@@ -99,6 +111,41 @@ int main(void)
 					{
 						child3 = sub;
 						printf("child3 = %d\n", child3);
+
+						// Calculations
+							while (cont == 1)
+							{
+								sleep(1);
+								srand(time(NULL) + 2);
+								fuel = fuel - ( rand() % 100 + 100 );
+								sleep(1);
+								if (fuel <= 0)
+								{
+									cont = 0;
+									fprintf(file, "mission unsuccessful\n");
+									exit(0);
+								}
+								else if (missiles <= 0)
+									ret = 1;
+	
+								sleep(1);
+								tm_info = localtime(&t);
+								strftime(outTime, 26, "Time: %H:%M:%S\n", tm_info);
+								fprintf(file, "%s\n", outTime);
+	
+								if (ret == 0)
+								{
+									srand(time(NULL) + 3);
+									dist_go = dist_go + ( rand() % 5 + 5);
+									fprintf(file, "\tBomber %d to base, %d gallons left, %d bombs left,\n\t%d miles from base.\n", 3, fuel, missiles, dist_go);
+								}
+								else
+								{
+									srand(time(NULL) + 4);
+									dist_ret = dist_ret - ( rand() % 5 + 3);
+									fprintf(file, "\tBomber %d to base, %d gallons left, %d bombs left,\n\t%d miles from base.\n", 3, fuel, missiles, dist_ret);
+								}
+							}
 					}
 				}
 				else	// child2
@@ -116,5 +163,9 @@ int main(void)
 		}
 		sleep(1);
 
+
+	free(term);
+
 	return 0;
 }
+
